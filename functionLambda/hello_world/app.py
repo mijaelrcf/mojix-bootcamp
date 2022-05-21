42213fa4678b628431bfa2c3284fd012c69c2a59
mijaelrcf <mijael.rcf@gmail.com>
2022-05-20 22:02:05 -0400

    name with UUID

--- functionLambda/hello_world/app.py
+++ functionLambda/hello_world/app.py
@@ -1,10 +1,14 @@
 import boto3
+import uuid
 
 def upload_to_aws(local_file, s3_file):
     s3 = boto3.client('s3')
+    
+    nameFileSplit = s3_file.split(".")
+    nameFile = uuid.uuid4() + "." nameFileSplit[1]
 
     try:
-        s3.upload_file(local_file, os.environ['BUCKET_NAME'], s3_file)
+        s3.upload_file(local_file, os.environ['BUCKET_NAME'], nameFile)
         url = s3.generate_presigned_url(
             ClientMethod='get_object',
             Params={
